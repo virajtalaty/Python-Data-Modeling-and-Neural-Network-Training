b5598ce0e72327c7376e8a9f293c9702e3ddb9fc
mouldi <mouldi.nouri@gmail.com>
2018-10-14 16:12:39 -0700

    changing to ceil

--- FileReader.py
+++ FileReader.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import os
+import math as m
 
 data = pd.read_table('data.sas', usecols=['Velocity', 'LanePos', 'SpeedLimit', 'Steer', 'Accel', 'Brake', 'LongAccel', 'HeadwayTime', 'HeadwayDist'])
 local_path = "DataSets/"
@@ -11,10 +12,7 @@ offset = file_length // 5
 
 # iteration on data sets and splitting the file
 
-if file_length % offset == 0:
-    chuncks_length = range(int(file_length/offset))
-else:
-    chuncks_length = range(int(file_length/offset)+1)
+chuncks_length = range(m.ceil(file_length/offset))
 
 for i in chuncks_length:
     df = data[i*offset: (i+1)*offset]
