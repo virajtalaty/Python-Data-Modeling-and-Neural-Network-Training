fc713534afef5ef36a9c757fe933870738bdfedb
mouldi <mouldi.nouri@gmail.com>
2018-10-14 01:58:39 -0700

    splitting the main data file

--- FileReader.py
+++ FileReader.py
@@ -1,6 +1,24 @@
 import pandas as pd
+import os
 
-data = pd.read_table('data.sas', usecols=[14, 2, 5])
+data = pd.read_table('data.sas', usecols=['Velocity', 'LanePos', 'SpeedLimit', 'Steer', 'Accel', 'Brake', 'LongAccel', 'HeadwayTime', 'HeadwayDist'])
+local_path = "DataSets/"
+# print(data)
 
-print(data)
+file_length = len(data)
+
+offset = file_length // 5
+
+# iteration on data sets and splitting the file
+
+if file_length % offset == 0:
+    chuncks_length = range(int(file_length/offset))
+else:
+    chuncks_length = range(int(file_length/offset)+1)
+
+for i in chuncks_length:
+    df = data[i*offset: (i+1)*offset]
+    df.to_csv(os.path.join(local_path, 'splittedfile_chunk_{}'.format(i)))
+
+
 
